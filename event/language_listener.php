6f7743642c609224203e5c43bf1e809e392511c2
mundophpbb <32120049+mundophpbb@users.noreply.github.com>
2026-01-28 19:09:45 -0300

    Add files via upload

--- event/language_listener.php
+++ event/language_listener.php
@@ -6,14 +6,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class language_listener implements EventSubscriberInterface
 {
-    protected $language;
-
-    public function __construct(\phpbb\language\language $language)
-    {
-        $this->language = $language;
-    }
-
-    static public function getSubscribedEvents()
+    public static function getSubscribedEvents()
     {
         return [
             'core.user_setup' => 'load_language_on_setup',
@@ -22,10 +15,11 @@ class language_listener implements EventSubscriberInterface
 
     public function load_language_on_setup($event)
     {
-        // Carrega apenas no ACP para evitar overhead no frontend
-        if (defined('ADMIN_START'))
-        {
-            $this->language->add_lang('acp_language', 'mundophpbb/topictranslatesingle');
-        }
+        $lang_set_ext = $event['lang_set_ext'];
+        $lang_set_ext[] = [
+            'ext_name' => 'mundophpbb/topictranslatesingle',
+            'lang_set' => 'common',
+        ];
+        $event['lang_set_ext'] = $lang_set_ext;
     }
 }
